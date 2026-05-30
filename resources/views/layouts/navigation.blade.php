520c08a5d69a79d3d1d277d7c89ebdc8dc03d2ce
haruna <yngwiejohannepunsalan@gmail.com>
2026-05-30 17:58:07 +0800

    [FE] style (nav): match desktop's nav links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -156,10 +156,10 @@
                     {{ __('Home') }}
                 </x-responsive-nav-link>
 
-                <x-responsive-nav-link :href="route('profiles.show', Auth::id())"
-                    :active="request()->routeIs('profiles.show')">
-                    <i class="fa-regular fa-user"></i>
-                    {{ __('Profile') }}
+                <x-responsive-nav-link :href="route('connections.index')"
+                    :active="request()->routeIs('connections.*')">
+                    <i class="fa-solid fa-users"></i>
+                    {{ __('Connections') }}
                 </x-responsive-nav-link>
 
                 <x-responsive-nav-link :href="route('communities.index')" :active="request()->routeIs('communities.*')">
