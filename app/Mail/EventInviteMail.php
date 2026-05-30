1f055da21279c61185cb5a3ca9db94c8bcb6f64f
haruna <yngwiejohannepunsalan@gmail.com>
2026-05-31 05:09:25 +0800

    [FE+BE] feat (posts): add text/media/event post types with event auto-invites

--- app/Mail/EventInviteMail.php
+++ app/Mail/EventInviteMail.php
@@ -4,16 +4,12 @@ namespace App\Mail;
 
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
-class EventInviteMail extends Mailable implements ShouldQueue
+class EventInviteMail extends Mailable
 {
-    use Queueable, SerializesModels;
 
     public function __construct(
         public Post $post,
